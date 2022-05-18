ece82b8e4da6c1105af18b2a8dbca583c508f522
Achu Ajesh <achuv37@gmail.com>
2022-05-18 09:08:19 -0400

    modified the server file

--- server.js
+++ server.js
@@ -1,82 +1,39 @@
-/*
-==========
-| SERVER |
-==================================================================================================================================
-
-? @collaborators:        
-                        * Aswathy Ajesh
-                        * Jordan Weston
-                        * Allen Bey
-                        * William Horn
-
-? @doc-name:            server.js
-? @doc-created:         05/17/2022
-? @doc-modified:        05/17/2022
-
-==================================================================================================================================
-
-? @doc-info
-==================
-| ABOUT DOCUMENT |
-==================================================================================================================================
-
-This program file is responsible for handling the initialization of all back-end functionality and uses the Express.js server
-framework to operate. learn more about Express.js here: https://expressjs.com/
-
-==================================================================================================================================
-
-? @doc-todo
-=================
-| DOCUMENT TODO |
-==================================================================================================================================
-
-todo:   add sequelize connection and sync it with server -Will [05/17/2022]
-        !Incomplete
-
-todo:   import express-handlebars and set-up middleware -Will [05/17/2022]
-        *Completed -Will [05/17/2022]
-
-todo:   create 'views' folder w/ corresponding handlebars files for each route in 'controllers' -Will [05/17/2022]
-        *Completed -Will [05/17/2022]
-
-==================================================================================================================================
-*/
-
-/* ---------------------------- */
-/* Import Environment Variables */
-/* ---------------------------- */
-require('dotenv').config();
-
-/* -------------- */
-/* Import Modules */
-/* -------------- */
+const path = require('path');
 const express = require('express');
-const { engine: renderEngine } = require('express-handlebars');
-const routes = require('./controllers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
-/* --------------------------------- */
-/* Application Set-Up and Middleware */
-/* --------------------------------- */
-// app set-up
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
-// app middleware/network
-app.use(express.json());                        // allow requests to accept json 
-app.use(express.static('public'));              // front-end file system
-app.use(express.urlencoded({extended: true}));  // allow for nested objects in body requests
+const sequelize = require('./config/connection.js');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// internal routes
-app.use(routes);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// render engine
-app.engine('handlebars', renderEngine());
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-/* ------------ */
-/* Start Server */
-/* ------------ */
-app.listen(PORT, err => {
-    if (err) throw err;
-    console.log('Server running on port:', PORT);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
